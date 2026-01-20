578db0835520768150b4837b115f322211220814
NERDHEAD <nerdlab.kr@gmail.com>
2026-01-20 14:59:22 +0900

    feat: 런쳐 UI 구성 - 8

--- src/renderer/components/GameStartButton.tsx
+++ src/renderer/components/GameStartButton.tsx
@@ -20,6 +20,49 @@ const GameStartButton: React.FC<GameStartButtonProps> = ({
       onClick={onClick}
       style={style}
     >
+      {/* Defined SVG Filter for Chiseled 3D Effect */}
+      <svg
+        width="0"
+        height="0"
+        style={{
+          position: "absolute",
+          width: 0,
+          height: 0,
+          pointerEvents: "none",
+        }}
+      >
+        <defs>
+          <filter id="gold-bevel" x="-50%" y="-50%" width="200%" height="200%">
+            <feGaussianBlur in="SourceAlpha" stdDeviation="0.8" result="blur" />
+            <feSpecularLighting
+              in="blur"
+              surfaceScale="4"
+              specularConstant="2.2"
+              specularExponent="40"
+              lightingColor="#ffffff"
+              result="specular"
+            >
+              <fePointLight x="-5000" y="-10000" z="8000" />
+            </feSpecularLighting>
+            <feComposite
+              in="specular"
+              in2="SourceAlpha"
+              operator="in"
+              result="specular-masked"
+            />
+            <feComposite
+              in="SourceGraphic"
+              in2="specular-masked"
+              operator="arithmetic"
+              k1="0"
+              k2="1"
+              k3="1"
+              k4="0"
+            />
+          </filter>
+        </defs>
+      </svg>
+
       <span className="hover-overlay"></span>
       <span className="main-start__text">{label}</span>
     </button>
